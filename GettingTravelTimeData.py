5466de78b78e52f2887b344adcb97a3733dda964
Apoorba Bibeka <abibeka@kittelson.com>
2019-08-08 13:18:18 -0500

    Update GettingTravelTimeData.py

--- GettingTravelTimeData.py
+++ GettingTravelTimeData.py
@@ -4,22 +4,117 @@ Created on Wed Aug  7 15:25:37 2019
 
 @author: abibeka
 """
+#0.0 Housekeeping. Clear variable space
+from IPython import get_ipython  #run magic commands
+ipython = get_ipython()
+ipython.magic("reset -f")
+ipython = get_ipython()
+
+
 from bs4 import BeautifulSoup
 import pandas as pd
 from os import path
 import geopy.distance
 import numpy as np 
+import subprocess #Need for opening Excel file from Python
 
+
+
+def getDist(row):
+    RowLatLong = tuple(row[['Lat','Long']].values.ravel())
+    Names = ['LPGA@Tomoka','LPGA@I95-SB-Ramp','LPGA@I95-NB-Ramp','LPGA@Technology','LPGA@Willamson','LPGA@Clyde-Morris']
+    Dist =[]
+    for N1 in Names:
+        Indx = DatLaLong.index[DatLaLong.Name==N1]
+        Point = tuple(DatLaLong.loc[Indx,['Lat','Long']].values.ravel())
+        Dist.append(geopy.distance.distance(Point,RowLatLong).feet)
+    return pd.Series(Dist,index=['D1','D2','D3','D4','D5','D6'])
+
+def getDist_Fun2(row):
+        RowLatLong = tuple(row[['Lat','Long']].values.ravel())
+        NextLatLong = tuple(row[['LatShift','LongShift']].values.ravel())
+        if(row["LatShift"]==row["LatShift"]):
+            return geopy.distance.distance(NextLatLong,RowLatLong).feet
+        else: return 0
+        
+def GetSummaryData(RawData, DatLatLong, Runs=[5,7,9,11,13],Dir='EB'):
+    
+    if(Dir == 'EB'):
+        #EB Heading = 60 Deg  #Runs Based on ZHB File
+        EB_Mask = (RawData.Run.isin(Runs)) & (RawData.Heading.between(30,100))
+        DataSub = RawData[EB_Mask]
+    else: 
+        WB_Mask = (RawData.Run.isin(Runs)) & (~RawData.Heading.between(30,100))
+        DataSub = RawData[WB_Mask]
+
+        
+    DataSub[['D1','D2','D3','D4','D5','D6']] = DataSub[['Lat','Long']].apply(getDist,axis=1)
+    DataSub.reset_index(inplace=True)
+    DataSub['ClosestInt'] = np.nan
+    for i in ['D1','D2','D3','D4','D5','D6']:
+        Indx = DataSub.groupby('Run')[i].idxmin() #Find the location of D1, D2......D6
+        DataSub.loc[Indx,'ClosestInt'] = i 
+        
+    DataSub.sort_values(['Run','Date_Time'],inplace=True) # Get sorted to shift columns
+    DataSub[['DateTShift','LatShift','LongShift']] = DataSub.groupby('Run')['Date_Time','Lat','Long'].shift(-1)
+    
+    # Get the distance, speed and time just based on end points 
+    DataBoundary = DataSub[~DataSub.ClosestInt.isna()]
+    DataBoundary = DataBoundary[['Run','ClosestInt','Date_Time','Lat','Long']]
+    DataBoundary = DataBoundary.sort_values(['Run','Date_Time']).reset_index(drop=True)
+    CorSumDataBoundary = DataBoundary[DataBoundary.ClosestInt.isin(['D1','D6'])].reset_index(drop=True)
+    DataBoundary[['DateTimeNextInt','LatShift','LongShift']] = DataBoundary.groupby('Run')['Date_Time','Lat','Long'].shift(-1)
+    DataBoundary = DataBoundary[~DataBoundary.LatShift.isna()]
+    DataBoundary.loc[:,"EndPointTimeDiff"] = (DataBoundary.DateTimeNextInt-DataBoundary.Date_Time).dt.total_seconds()
+    DataBoundary.loc[:,'DistBtwEndPnt'] = DataBoundary[['Lat','Long','LatShift','LongShift']].apply(getDist_Fun2,axis=1)
+    DataBoundary = DataBoundary[DataBoundary.DistBtwEndPnt>=900] #We know distance between each intersection is at least 900 ft.
+    # Get the distance, speed and time just based on end points     
+    CorSumDataBoundary[['DateTimeNextInt','LatShift','LongShift']] = CorSumDataBoundary.groupby('Run')['Date_Time','Lat','Long'].shift(-1)
+    CorSumDataBoundary = CorSumDataBoundary[~CorSumDataBoundary.LatShift.isna()]
+    CorSumDataBoundary.loc[:,"EndPointTimeDiff"] = (CorSumDataBoundary.DateTimeNextInt-CorSumDataBoundary.Date_Time).dt.total_seconds()
+    CorSumDataBoundary.loc[:,'DistBtwEndPnt'] = CorSumDataBoundary[['Lat','Long','LatShift','LongShift']].apply(getDist_Fun2,axis=1)
+    CorSumDataBoundary = CorSumDataBoundary.drop(columns='ClosestInt')
+    
+    # Subset the data and then use forward fill to set correct labels
+    ListDa =[]    
+    for key,group_df in DataSub.groupby('Run'):
+        RefDT1 = group_df.Date_Time[group_df.ClosestInt == 'D1'].values[0]
+        RefDT2 = group_df.Date_Time[group_df.ClosestInt == 'D6'].values[0]
+        if(Dir == 'EB'):
+            mask  = (group_df.Date_Time>=RefDT1) & (group_df.Date_Time<=RefDT2)
+        else:
+            mask  = (group_df.Date_Time<=RefDT1) & (group_df.Date_Time>=RefDT2)
+        tempDat = group_df[mask]
+        tempDat['ClosestInt'] = tempDat.ClosestInt.fillna(method='ffill')
+        ListDa.append(tempDat)
+    CleanDat = pd.concat(ListDa)
+    CleanDat.reset_index(drop=True,inplace=True)
+    CleanDat.loc[:,"TimeDiff"] = (CleanDat.DateTShift-CleanDat.Date_Time).dt.total_seconds()
+
+    CleanDat.loc[:,'Speed_fps'] = CleanDat.Speed *1.47
+    CleanDat.loc[:,'DistFromSpd'] = CleanDat.Speed_fps * CleanDat.TimeDiff
+    CleanDat.loc[:,'DistBtwPnt'] = CleanDat[['Lat','Long','LatShift','LongShift']].apply(getDist_Fun2,axis=1)
+    SumIntDat= CleanDat.groupby(['Run','ClosestInt']).agg({'DistFromSpd':'sum','DistBtwPnt':'sum','TimeDiff':'sum'})
+    SumIntDat = SumIntDat.reset_index(drop=False)
+    FinIntDat = SumIntDat.groupby(['ClosestInt']).agg({'DistFromSpd':'mean','DistBtwPnt':'mean','TimeDiff':'mean'})
+    FinIntDat.loc[:,'SMS_mph'] = FinIntDat.DistBtwPnt/ FinIntDat.TimeDiff/ 1.47
+    LeftVars = ['Run','ClosestInt']
+    SumIntDat = pd.merge(DataBoundary,SumIntDat,left_on = LeftVars, right_on = LeftVars, how='left')
+    SumCorDat = CleanDat.groupby(['Run']).agg({'DistFromSpd':'sum','DistBtwPnt':'sum','TimeDiff':'sum'})
+    SumCorDat.loc[:,'SMS'] = SumCorDat.DistBtwPnt/SumCorDat.TimeDiff
+    SumCorDat = SumCorDat.reset_index(drop=False)
+    SumCorDat = pd.merge(CorSumDataBoundary,SumCorDat,left_on = 'Run', right_on = 'Run', how='left')
+    CorridorSMS_mph = SumCorDat.DistBtwPnt.sum()/ SumCorDat.TimeDiff.sum()/ 1.47
+    FinCorDat = pd.DataFrame({'MeanDist':[SumCorDat.DistBtwPnt.mean()], 'MeanTime': [SumCorDat.TimeDiff.mean()], 'SMS_mph': [CorridorSMS_mph]})
+    return(SumIntDat,SumCorDat,FinIntDat,FinCorDat,CleanDat)
+    
+    
 kml_file = path.join(r'C:\Users\abibeka\OneDrive - Kittelson & Associates, Inc\Documents\LPGA\VISSIM-Files\LPGA-Intersections.kml')
 
 with open(kml_file) as fp:
-    soup = BeautifulSoup(fp)
+    soup = BeautifulSoup(fp,features='lxml')
 #print(soup.prettify())
 
-soup.placemark.find('name').string
-soup.placemark.lookat.longitude.string
-soup.placemark.lookat.latitude.string
-soup.placemark.coordinates.string.split(',')[:2]
 DatLaLong = pd.DataFrame()
 for item in soup.find_all('placemark'):
     Name =item.find('name').string
@@ -29,8 +124,8 @@ for item in soup.find_all('placemark'):
     tempDa = pd.DataFrame({'Name': [Name],'Lat': [Lat],'Long': [Long]})
     DatLaLong= pd.concat([DatLaLong,tempDa])
     print(Name,Lat,Long)
-
-
+# DatLatLong has ref lat longs
+DatLaLong.reset_index(inplace=True)
 TTRunFi = path.join(r'C:\Users\abibeka\OneDrive - Kittelson & Associates, Inc\Documents\LPGA\VISSIM-Files\LPGA Travel Time Data-AXB.xlsx')
     #Get the excel file in memory. Helps with iterating over sheet names
 x1 = pd.ExcelFile(TTRunFi)
@@ -38,77 +133,97 @@ x1.sheet_names
 AMTTRawDa= x1.parse('AM Raw',parse_dates=[['Date', 'Time']])
 AMTTRawDa = AMTTRawDa[['Run','Date_Time','Speed','Heading','Lat','Long']]
 
-#EB Heading = 60 Deg
-EB_Mask = (AMTTRawDa.Run.isin([5,7,9,11,13])) & (AMTTRawDa.Heading.between(30,100))
-EBAMTTRawDa = AMTTRawDa[EB_Mask]
+PMTTRawDa= x1.parse('PM Raw',parse_dates=[['Date', 'Time']])
+PMTTRawDa = PMTTRawDa[['Run','Date_Time','Speed','Heading','Lat','Long']]
 
-# DatLatLong has ref lat longs
-DatLaLong.reset_index(inplace=True)
 
-def getDist(row):
-    RowLatLong = tuple(row[['Lat','Long']].values.ravel())
-    Indx1 = DatLaLong.index[DatLaLong.Name=='LPGA@Tomoka']
-    Indx2 = DatLaLong.index[DatLaLong.Name=='LPGA@I95-SB-Ramp']
-    Indx3 = DatLaLong.index[DatLaLong.Name=='LPGA@I95-NB-Ramp']
-    Indx4 = DatLaLong.index[DatLaLong.Name=='LPGA@Technology']
-    Indx5 = DatLaLong.index[DatLaLong.Name=='LPGA@Willamson']
-    Indx6 = DatLaLong.index[DatLaLong.Name=='LPGA@Clyde-Morris']
-    Point1 = tuple(DatLaLong.loc[Indx1,['Lat','Long']].values.ravel())
-    Point2 = tuple(DatLaLong.loc[Indx2,['Lat','Long']].values.ravel())
-    Point3 = tuple(DatLaLong.loc[Indx3,['Lat','Long']].values.ravel())
-    Point4 = tuple(DatLaLong.loc[Indx4,['Lat','Long']].values.ravel())
-    Point5 = tuple(DatLaLong.loc[Indx5,['Lat','Long']].values.ravel())
-    Point6 = tuple(DatLaLong.loc[Indx6,['Lat','Long']].values.ravel())
-    D1 = geopy.distance.distance(Point1,RowLatLong).feet
-    D2 = geopy.distance.distance(Point2,RowLatLong).feet
-    D3 = geopy.distance.distance(Point3,RowLatLong).feet
-    D4 = geopy.distance.distance(Point4,RowLatLong).feet
-    D5 = geopy.distance.distance(Point5,RowLatLong).feet
-    D6 = geopy.distance.distance(Point6,RowLatLong).feet
-    return pd.Series([D1,D2,D3,D4,D5,D6],index=['D1','D2','D3','D4','D5','D6'])
+Names = ['LPGA@Tomoka','LPGA@I95-SB-Ramp','LPGA@I95-NB-Ramp','LPGA@Technology','LPGA@Willamson','LPGA@Clyde-Morris']
+
+
+
+FinData = {}
+
+Debug = False
+if (Debug): 
+    #AM_EB Zach Coordinates
+    DatLaLong.loc[DatLaLong.Name=='LPGA@Tomoka',['Lat','Long']] = ['29.217025','-81.109812']
+    DatLaLong.loc[DatLaLong.Name=='LPGA@Clyde-Morris',['Lat','Long']] = ['29.227215','-81.085725']
+    Run1 = np.arange(0,100)
+else:
+    Run1 = [3,5,7,9,11,13,15]
+FinData['AM_EB'] =GetSummaryData(RawData=AMTTRawDa, DatLatLong = DatLaLong, Runs=Run1,Dir='EB')
+FinData['AM_EB'][0]
+FinData['AM_EB'][1]
+FinData['AM_EB'][1].DistBtwPnt.sum() / FinData['AM_EB'][1].TimeDiff.sum() /1.47
+
+
+if (Debug): 
+    #AM_WB Zach Coordinates
+    DatLaLong.loc[DatLaLong.Name=='LPGA@Clyde-Morris',['Lat','Long']] = ['29.22972','-81.08033']
+    DatLaLong.loc[DatLaLong.Name=='LPGA@Tomoka',['Lat','Long']] = ['29.21753','-81.108792']
+    Run1 = np.arange(0,100)
+else:
+    Run1 = [4,6,8,10,12,14]
+FinData['AM_WB'] =GetSummaryData(RawData=AMTTRawDa, DatLatLong = DatLaLong, Runs=Run1 ,Dir='WB')
+FinData['AM_WB'][0]
+FinData['AM_WB'][1]
+FinData['AM_WB'][1].DistBtwPnt.sum() / FinData['AM_WB'][1].TimeDiff.sum() /1.47
+
+
+
+if (Debug): 
+    #PM_EB Zach Coordinates
+    DatLaLong.loc[DatLaLong.Name=='LPGA@Tomoka',['Lat','Long']] = ['29.217112','-81.109623']
+    DatLaLong.loc[DatLaLong.Name=='LPGA@Clyde-Morris',['Lat','Long']] = ['29.227387','-81.085297']
+    Run1 = [1,3,5,7,9,11,13]
+else:
+    Run1 = [5,7,9,11]
+FinData['PM_EB'] =GetSummaryData(RawData=PMTTRawDa, DatLatLong = DatLaLong, Runs=Run1,Dir='EB')
+FinData['PM_EB'][0]
+FinData['PM_EB'][1]
+FinData['PM_EB'][1].DistBtwPnt.sum() / FinData['PM_EB'][1].TimeDiff.sum() /1.47
+
+if (Debug): 
+    #PM_WB Zach Coordinates
+    DatLaLong.loc[DatLaLong.Name=='LPGA@Clyde-Morris',['Lat','Long']] = ['29.229435','-81.080913']
+    DatLaLong.loc[DatLaLong.Name=='LPGA@Tomoka',['Lat','Long']] = ['29.217562','-81.108732']
+    Run1 = [2,4,6,8,10,12,14,16,17]
+else:
+    Run1 = [4,6,8,10,12]
+FinData['PM_WB'] = GetSummaryData(RawData=PMTTRawDa, DatLatLong = DatLaLong, Runs=Run1,Dir='WB')
+
+FinData['PM_WB'][1].DistBtwPnt.sum() / FinData['PM_WB'][1].TimeDiff.sum() /1.47
+FinData['PM_WB'][0]
+FinData['PM_WB'][1]
+FinData['PM_WB'][1].DistBtwPnt.sum() / FinData['PM_WB'][1].TimeDiff.sum() /1.47
+#Debug function manually
+#RawData=AMTTRawDa; DatLatLong = DatLaLong; Runs=[3,5,7,9,11,13]; Dir='EB'
+
+
+TTDebug = path.join(r'C:\Users\abibeka\OneDrive - Kittelson & Associates, Inc\Documents\LPGA\VISSIM-Files\ComparisionValidat_Zach_TT.xlsx')
+TTOut = path.join(r'C:\Users\abibeka\OneDrive - Kittelson & Associates, Inc\Documents\LPGA\VISSIM-Files\TTData-by-Intersection.xlsx')
+
+if (Debug): 
+    writer=pd.ExcelWriter(TTDebug)
+    for key in FinData.keys():
+        FinData[key][1].to_excel(writer,key)
+        startrow1 = writer.sheets[key].max_row
+        FinData[key][3].to_excel(writer,key, startrow = startrow1+2)
+    writer.save()
+else:
+    writer=pd.ExcelWriter(TTOut)
+    for key in FinData.keys():
+        FinData[key][0].to_excel(writer,key)
+        startrow1 = writer.sheets[key].max_row
+        FinData[key][2].to_excel(writer,key, startrow = startrow1+2)
+    writer.save()
+ 
     
-EBAMTTRawDa[['D1','D2','D3','D4','D5','D6']] = EBAMTTRawDa[['Lat','Long']].apply(getDist,axis=1)
+# Open excel file from python 
+subprocess.Popen([TTOut],shell=True)  
 
-EBAMTTRawDa.reset_index(inplace=True)
-EBAMTTRawDa['ClosestInt'] = np.nan
-for i in ['D1','D2','D3','D4','D5','D6']:
-    Indx = EBAMTTRawDa.groupby('Run')[i].idxmin()
-    EBAMTTRawDa.loc[Indx,'ClosestInt'] = i 
-   
+subprocess.Popen([TTDebug],shell=True)  
 
-EBAMTTRawDa.sort_values(['Run','Date_Time'],inplace=True)
+subprocess.Popen([TTRunFi],shell=True)  
 
 
-EBAMTTRawDa[['DateTShift','LatShift','LongShift']] = EBAMTTRawDa.groupby('Run')['Date_Time','Lat','Long'].shift(-1)
-
-
-ListDa =[]
-for key,group_df in EBAMTTRawDa.groupby('Run'):
-    RefDT1 = group_df.Date_Time[group_df.ClosestInt == 'D1'].values[0]
-    RefDT2 = group_df.Date_Time[group_df.ClosestInt == 'D6'].values[0]
-    mask  = (group_df.Date_Time>=RefDT1) & (group_df.Date_Time<=RefDT2)
-    tempDat = group_df[mask]
-    tempDat['ClosestInt'] = tempDat.ClosestInt.fillna(method='ffill')
-    ListDa.append(tempDat)
-
-EBDat = pd.concat(ListDa)
-
-EBDat.loc[:,"TimeDiff"] = (EBDat.DateTShift-EBDat.Date_Time).dt.total_seconds()
-
-
-def getDist_Fun2(row):
-    RowLatLong = tuple(row[['Lat','Long']].values.ravel())
-    NextLatLong = tuple(row[['LatShift','LongShift']].values.ravel())
-    if(row["LatShift"]==row["LatShift"]):
-        return geopy.distance.distance(NextLatLong,RowLatLong).feet
-    else: return 0
-
-EBDat.loc[:,'Speed_fps'] = EBDat.Speed *1.47
-EBDat.loc[:,'DistFromSpd'] = EBDat.Speed_fps * EBDat.TimeDiff
-EBDat.loc[:,'DistBtwPnt'] = EBDat[['Lat','Long','LatShift','LongShift']].apply(getDist_Fun2,axis=1)
-
-
-
-EBDat.groupby(['Run','ClosestInt']).agg({'DistFromSpd':'sum','DistBtwPnt':'sum','TimeDiff':'sum'})
-
-EBDat.groupby(['Run']).agg({'DistFromSpd':'sum','DistBtwPnt':'sum','TimeDiff':'sum'})
