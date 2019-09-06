aaaf2bb938d1428fb3ce139dcd81cb9c197067a1
Apoorba Bibeka <apoorb2510@gmail.com>
2019-09-06 15:32:13 -0400

    Clean Code

--- After-Validation-TT-Processing.py
+++ After-Validation-TT-Processing.py
@@ -20,13 +20,6 @@ import numpy as np
 import subprocess
 import seaborn as sns
 import matplotlib.pyplot as plt
-# VISSIM File
-#*********************************************************************************
-PathToExist = r'C:\Users\abibeka\OneDrive - Kittelson & Associates, Inc\Documents\LPGA\VISSIM-Files\VISSIM - V2\Existing'
-ExistingPMfi = '20834_Existing_PM--C1C2C3C4C5C6C7_Vehicle Travel Time Results.att'
-ExistingPMfi = os.path.join(PathToExist,ExistingPMfi)
-ExistingAMfi ='20834_Existing_AM--C1C2aC3C4C5C6C7C8_Vehicle Travel Time Results.att'
-ExistingAMfi = os.path.join(PathToExist,ExistingAMfi)
 
 
 
@@ -83,9 +76,15 @@ def PreProcessVissimTT(file, SegIN):
     ExistingAMDat.loc[:,"VissimSMS"] = (ExistingAMDat['Len']/ExistingAMDat['VissimTT']/1.47).round(1)
     mask1 = ~ExistingAMDat['VEHICLETRAVELTIMEMEASUREMENT'].isin([13,14])
     TTData = ExistingAMDat[mask1]
+    TTData.loc[:,"VissimSMS"] = TTData.groupby(['TTSegNm'])['VissimSMS'].transform(lambda v: v.ffill())
+
     return(TTData)
 
 # Get the Weighted Density by Segments
+#Debug:
+#file = NoBuildAMfi
+#SegKeyVal = TTSegLaneDat
+#SegIN = SegIn1
 def PreProcessVissimDensity(file, SegKeyVal):
     '''
     file : VISSIM Results file
@@ -104,12 +103,17 @@ def PreProcessVissimDensity(file, SegKeyVal):
     ExistingAMDat.loc[:,"VissimSMS"] = (ExistingAMDat['Len']/ExistingAMDat['VissimTT']/1.47).round(1)
     #Get flow rate and density
     ExistingAMDat.loc[:,'FlowRate'] = ExistingAMDat.Veh *4
+    ExistingAMDat.loc[:,'FlowRate_PerLane'] = ExistingAMDat.Veh *4/ ExistingAMDat.NumLanes
+    ExistingAMDat.loc[:,"VissimSMS"] = (ExistingAMDat['Len']/ExistingAMDat['VissimTT']/1.47).round(1)
     ExistingAMDat.loc[:,'Density'] = (ExistingAMDat.FlowRate/ ExistingAMDat.VissimSMS).round(1)
     ExistingAMDat.loc[:,'Density_Len_Lanes'] = ExistingAMDat.Density *ExistingAMDat.Len
     ExistingAMDat.loc[:,'Len_Lanes'] = ExistingAMDat.Len * ExistingAMDat.NumLanes
     ExistingAMDat.columns
-    DensityData = ExistingAMDat.groupby(['TIMEINT','SegName'])['Len_Lanes','Density_Len_Lanes'].sum().reset_index()
+    DensityData = ExistingAMDat.groupby(['TIMEINT','SegName']).aggregate({'Len_Lanes':'sum','Density_Len_Lanes':'sum','VissimSMS':'mean','FlowRate_PerLane':'mean'}).reset_index()
+    #DensityData = ExistingAMDat.groupby(['TIMEINT','SegName'])['Len_Lanes','Density_Len_Lanes'].sum().reset_index()
     DensityData.loc[:,'WeightedDensity'] = (DensityData.Density_Len_Lanes/ DensityData.Len_Lanes).round(1)
+    DensityData.loc[:,"WeightedDensity"] = DensityData.groupby(['SegName'])['WeightedDensity'].transform(lambda v: v.ffill())
+    DensityData.loc[:,"VissimSMS"] = DensityData.groupby(['SegName'])['VissimSMS'].transform(lambda v: v.ffill())
     DensityData = DensityData[['TIMEINT','SegName','WeightedDensity']]
     return(DensityData)
 
@@ -135,7 +139,67 @@ LPGASeg=['EB LPGA (Tomoka Rd to I-95 SB Ramp)',
     'WB LPGA (I-95 NB Ramp to I-95 SB Ramp)',
     'WB LPGA (I-95 SB Ramp to Tomoka Rd)']
 
+# VISSIM File
+#*********************************************************************************
+PathToFile= r'C:\Users\abibeka\OneDrive - Kittelson & Associates, Inc\Documents\LPGA\VISSIM-Files\VISSIM - V2'
+ExistingPMfi = r'Existing\20834_Existing_PM--C1C2C3C4C5C6C7_Vehicle Travel Time Results.att'
+ExistingPMfi = os.path.join(PathToFile,ExistingPMfi)
+ExistingAMfi =r'Existing\20834_Existing_AM--C1C2aC3C4C5C6C7C8_Vehicle Travel Time Results.att'
+ExistingAMfi = os.path.join(PathToFile,ExistingAMfi)
 
+
+
+NoBuildAMfi = r'No Build\20834_NoBuild_AM--C1C2C3C4C5C6C7C8_Vehicle Travel Time Results.att'
+NoBuildPMfi = r'No Build\20834_NoBuild_PM---C1C2C3C4C5C6C7C8_Vehicle Travel Time Results.att'
+BuildDCDI_AMfi = r'Build\DCDI\20834_Build1_6LnMod_AM---C1C2C3C4C5C6C7C8C9_Vehicle Travel Time Results.att'
+BuildDCDI_PMfi = r'Build\DCDI\20834_Build1_6LnMod_PM---C1C2C3C4C5C6C7C8C9_Vehicle Travel Time Results.att'
+BuildDCMI_AMfi = r'Build\DCMI\20834_DCMI_AM_20180808--C1C2C3C4C5C6C7C8_Vehicle Travel Time Results.att'
+BuildDCMI_PMfi = r'Build\DCMI\20834_DCMI_PM_20180808--C1C2C3C4C5C6C7C8_Vehicle Travel Time Results.att'
+
+NoBuildAMfi = os.path.join(PathToFile,NoBuildAMfi)
+NoBuildPMfi = os.path.join(PathToFile,NoBuildPMfi)
+BuildDCDI_AMfi = os.path.join(PathToFile,BuildDCDI_AMfi)
+BuildDCDI_PMfi = os.path.join(PathToFile,BuildDCDI_PMfi)
+BuildDCMI_AMfi = os.path.join(PathToFile,BuildDCMI_AMfi)
+BuildDCMI_PMfi = os.path.join(PathToFile,BuildDCMI_PMfi)
+
+#*******************************************************************************
+#*******************************************************************************
+# Which File are we analysising. Only make changes here and done:
+FileAM = ExistingAMfi
+FileAM  = NoBuildAMfi
+FileAM  = BuildDCDI_AMfi
+FileAM  = BuildDCMI_AMfi
+#File Being Processed
+#*******************************************************************************
+FileAM = BuildDCMI_AMfi
+#*******************************************************************************
+
+def switch_PMFile(argument):
+    switcher = {
+        ExistingAMfi: ExistingPMfi,
+        NoBuildAMfi: NoBuildPMfi,
+        BuildDCDI_AMfi: BuildDCDI_PMfi,
+        BuildDCMI_AMfi: BuildDCMI_PMfi
+    }
+    return switcher.get(argument, "Invalid Name")
+
+FilePM = switch_PMFile(FileAM)
+
+def switch_OutputTag(argument):
+    switcher = {
+        ExistingAMfi: "Existing",
+        NoBuildAMfi: "No-Build",
+        BuildDCDI_AMfi: "DCDI",
+        BuildDCMI_AMfi: "DCMI"
+    }
+    return switcher.get(argument, "Invalid Name")
+    
+OutputFolderName = switch_OutputTag(FileAM)
+
+if((FileAM == BuildDCDI_AMfi) | (FileAM == BuildDCMI_AMfi)):
+    AreResultsForBuild = True
+else: AreResultsForBuild = False
 # Get the time keys to convert Vissim intervals to Hours of the day
 PathToVis = r'C:\Users\abibeka\OneDrive - Kittelson & Associates, Inc\Documents\LPGA\VISSIM-Files'
 TimeConvFi  = 'TravelTimeKeyValuePairs.xlsx'
@@ -143,17 +207,21 @@ TimeConvFi = pd.ExcelFile(os.path.join(PathToVis,TimeConvFi))
 TimeConvFi.sheet_names
 TimeKeys = TimeConvFi.parse('TimeVissimKey')
 
-# Get The Travel time (TT) segment and # of lane data
-TTSegLaneDat = TimeConvFi.parse('Existing')
-TTSegLaneDat = TTSegLaneDat.dropna()
+if(AreResultsForBuild):
+    TTSegLaneDat = TimeConvFi.parse('Build')
+    TTSegLaneDat = TTSegLaneDat.dropna()
+else:
+    # Get The Travel time (TT) segment and # of lane data
+    TTSegLaneDat = TimeConvFi.parse('Existing')
+    TTSegLaneDat = TTSegLaneDat.dropna()
+
 
 #Summarize the Travel time for AM and PM. Concatenate the 2 datasets.
 SegIn1 = I95_Segs+ LPGASeg
-file = ExistingAMfi
-TTAM = PreProcessVissimTT(ExistingAMfi,SegIn1)
-TTPM = PreProcessVissimTT(ExistingPMfi,SegIn1)
-DenAM = PreProcessVissimDensity(ExistingAMfi,TTSegLaneDat)
-DenPM = PreProcessVissimDensity(ExistingPMfi,TTSegLaneDat)
+TTAM = PreProcessVissimTT(FileAM,SegIn1)
+TTPM = PreProcessVissimTT(FilePM,SegIn1)
+DenAM = PreProcessVissimDensity(FileAM,TTSegLaneDat)
+DenPM = PreProcessVissimDensity(FilePM,TTSegLaneDat)
 TTAM = TTAM.merge(DenAM,left_on=['TIMEINT','TTSegNm'],right_on=['TIMEINT','SegName'],how='left')
 TTPM = TTPM.merge(DenPM,left_on=['TIMEINT','TTSegNm'],right_on=['TIMEINT','SegName'],how='left')
 TTAM.loc[:,'AnalysisPeriod'] = 'AM'
@@ -162,10 +230,19 @@ Vissim_TT = pd.concat([TTAM,TTPM],sort=False)
 Vissim_TT.columns
 Vissim_TT = Vissim_TT[['AnalysisPeriod','TIMEINT','TTSegNm','VissimSMS','WeightedDensity','Veh','Len']]
 
-# Convert Vissim intervals to Hours of the day
+#Correction-Oversaturated-Conditions
+# For 10 to 20 mph if the density > 45, just use it else replace with 160
+mask = (Vissim_TT.VissimSMS < 20) & (Vissim_TT.WeightedDensity < 45)
+Vissim_TT.loc[mask,"WeightedDensity"] = 160 # 33 ft. spacing
+# For < 10 mph replace all values with 160
+mask = (Vissim_TT.VissimSMS < 10) & (Vissim_TT.WeightedDensity < 200)
+Vissim_TT.loc[mask,"WeightedDensity"] = 160 # 33 ft. spacing
+Debug = Vissim_TT.loc[mask,"WeightedDensity"]
 
+# Convert Vissim intervals to Hours of the day
 lkey_ = ['AnalysisPeriod','TIMEINT']
 Vissim_TT = Vissim_TT.merge(TimeKeys,left_on = lkey_, right_on =lkey_,how = 'inner')
+
 
 
 #Sort the Travel Segments in Correct Order
@@ -197,9 +274,12 @@ Direction = ['NB','SB','EB','WB']
 PerfMeasure = ['VissimSMS','Density']
 t = 'AM'
 d = 'SB'
-PerfMes ='VissimSMS'
+PerfMes ='Density'
 (not((d=='EB' or d=='WB') & (PerfMes=='Density')))
-os.chdir(r'C:\Users\abibeka\OneDrive - Kittelson & Associates, Inc\Documents\LPGA\VISSIM-Files\Figs')
+if not os.path.exists(r'C:\Users\abibeka\OneDrive - Kittelson & Associates, Inc\Documents\LPGA\VISSIM-Files\Figs\{}'.format(OutputFolderName)):
+    os.makedirs(r'C:\Users\abibeka\OneDrive - Kittelson & Associates, Inc\Documents\LPGA\VISSIM-Files\Figs\{}'.format(OutputFolderName))
+os.chdir(r'C:\Users\abibeka\OneDrive - Kittelson & Associates, Inc\Documents\LPGA\VISSIM-Files\Figs\{}'.format(OutputFolderName))
+os.getcwd()
 DictFigs = {}
 idx = pd.IndexSlice
 #Color Palettes
@@ -211,10 +291,13 @@ for t in TimePer:
                 HeatMapDat = Vissim_TT.loc[idx[:,d,:], idx[t,['Time',PerfMes]]]
                 HeatMapDat = HeatMapDat.droplevel(0,axis=1).reset_index()
                 HeatMapDat = HeatMapDat.pivot('Time','TTSegNm',PerfMes)
+                HeatMapDat = HeatMapDat.astype('int32')
+                plt.figure()
                 if PerfMes=='Density':
-                    vmax_ = 65; vmin_ = 0
                     title_ = "Density (veh/mile/lane)"
                     colorBar_ = 'viridis_r'
+                    vmax_ = 160; vmin_ = 0
+                    g = sns.heatmap(HeatMapDat,cmap = colorBar_, linewidths=.5,vmin=vmin_, vmax=vmax_, annot=True,fmt="d")
                 else:   
                     title_ = "Space Mean Speed (mph)"
                     colorBar_ = 'viridis'
@@ -222,10 +305,11 @@ for t in TimePer:
                         vmax_ = 70; vmin_ = 0
                     else:
                         vmax_ = 45; vmin_ = 0
-                plt.figure()
+                    g = sns.heatmap(HeatMapDat,cmap = colorBar_, linewidths=.5, vmin=vmin_, vmax=vmax_, annot=True,fmt="d")
+                
                 # figure size in inches
                 #sns.set(rc={'figure.figsize':(6,10)})
-                g = sns.heatmap(HeatMapDat,cmap = colorBar_, linewidths=.5, vmin=vmin_, vmax=vmax_)
+                
                 g.set_xticklabels(rotation=30,labels = g.get_xticklabels(),ha='right')
                 g.set_title('{}—{}'.format(t,title_))
                 g.set_xlabel("")
@@ -233,7 +317,10 @@ for t in TimePer:
                 fig = g.get_figure()
                 fig.savefig("".join([t,d,PerfMes,'.jpg']),bbox_inches="tight")
 
-os.chdir(r'C:\Users\abibeka\OneDrive - Kittelson & Associates, Inc\Documents\LPGA\VISSIM-Files\Figs')
+
+if not os.path.exists(r'C:\Users\abibeka\OneDrive - Kittelson & Associates, Inc\Documents\LPGA\VISSIM-Files\Figs\{}'.format(OutputFolderName)):
+    os.makedirs(r'C:\Users\abibeka\OneDrive - Kittelson & Associates, Inc\Documents\LPGA\VISSIM-Files\Figs\{}'.format(OutputFolderName))
+os.chdir(r'C:\Users\abibeka\OneDrive - Kittelson & Associates, Inc\Documents\LPGA\VISSIM-Files\Figs\{}'.format(OutputFolderName))
 os.getcwd()
 t = 'AM'
 d = 'SB'
@@ -241,7 +328,7 @@ PerfMes ='VissimSMS'
 for t in TimePer:
     for d in Direction:
         for PerfMes in PerfMeasure:
-            os.chdir(r'C:\Users\abibeka\OneDrive - Kittelson & Associates, Inc\Documents\LPGA\VISSIM-Files\Figs')
+            os.chdir(r'C:\Users\abibeka\OneDrive - Kittelson & Associates, Inc\Documents\LPGA\VISSIM-Files\Figs\{}'.format(OutputFolderName))
             if not os.path.exists('{}/{}/{}'.format(t,d,PerfMes)):
                 os.makedirs('{}/{}/{}'.format(t,d,PerfMes))
             os.chdir('{}/{}/{}'.format(t,d,PerfMes))
@@ -258,7 +345,7 @@ for t in TimePer:
                         ax1.set_ylim(0,70)
                         ylab = "Space Mean Speed (mph)"
                     else: 
-                        ax1.set_ylim(0,65)
+                        ax1.set_ylim(0,170)
                         ylab = "Density (veh/mile/lane)"
 
                     ax1.set_title('{}—{}'.format(t,Tseg), y =1.05)
@@ -273,11 +360,14 @@ for t in TimePer:
 PathToOutFi = r'C:\Users\abibeka\OneDrive - Kittelson & Associates, Inc\Documents\LPGA\VISSIM-Files'
 OutFi = "AfterValidationRes.xlsx"
 OutFi = os.path.join(PathToOutFi,OutFi)
-writer=pd.ExcelWriter(OutFi)
-Vissim_LPGA.to_excel(writer, 'LPGA-SMS-Res')
-Vissim_I95.to_excel(writer,'I95-SMS&Density-Res')
+if(OutputFolderName =='Existing'):
+    writer=pd.ExcelWriter(OutFi)
+else:
+    writer=pd.ExcelWriter(OutFi, mode ='a')
+Vissim_LPGA.to_excel(writer, '{}-LPGA-SMS-Res'.format(OutputFolderName),na_rep='NaN')
+Vissim_I95.to_excel(writer,'{}-I95-SMS&Density-Res'.format(OutputFolderName),na_rep='NaN')
 writer.save()
 
-
-subprocess.Popen([OutFi],shell=True)  
+if(OutputFolderName == 'DCMI'):
+    subprocess.Popen([OutFi],shell=True)  
 
