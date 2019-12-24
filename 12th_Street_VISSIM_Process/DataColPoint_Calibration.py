37e35ebe5d4e6df6a46bc39120ad599e0b351248
Apoorba Bibeka <apoorb2510@gmail.com>
2019-12-24 09:14:29 -0500

    Update DataColPoint_Calibration.py

--- 12th_Street_VISSIM_Process/DataColPoint_Calibration.py
+++ 12th_Street_VISSIM_Process/DataColPoint_Calibration.py
@@ -16,7 +16,7 @@ ipython = get_ipython()
 
 import os
 import pandas as pd
-
+import subprocess 
 # Use consistent naming in VISSIM
 os.chdir(r'C:\Users\abibeka\OneDrive - Kittelson & Associates, Inc\Documents\12th-Street-TransitWay\Results')
 #os.chdir(r'H:\20\20548 - Arlington County Engineering On-Call\009 - 12th Street Transitway Extension\vissim\Results')
@@ -30,7 +30,7 @@ FieldDataColMap = x1.parse('FieldDataColMap')
 
 
     
-def PreProcessVissimDataCol(file,DatColMap = DatColMap,Peak= "AM Peak"):
+def PreProcessVissimDataCol(file,DatColMap = DatColMap,Peak= "AM Peak", SimRun = "AVG"):
     #Ignore the variable names. Don't actually mean ExistingAMDat
     # Define PM file. Would later use to figure out if a file for PM 
     # This is hard coding. Would break if the name of PM file is changed.
@@ -38,7 +38,7 @@ def PreProcessVissimDataCol(file,DatColMap = DatColMap,Peak= "AM Peak"):
     ExistingAMDat=pd.read_csv(file,sep =';',comment = "*",skiprows=1)
     ExistingAMDat.columns
     ExistingAMDat.rename(columns={'SPEEDAVGARITH(ALL)':'VissimAvgSpd'},inplace=True)
-    mask=ExistingAMDat["$DATACOLLECTIONMEASUREMENTEVALUATION:SIMRUN"]=="AVG"
+    mask=ExistingAMDat["$DATACOLLECTIONMEASUREMENTEVALUATION:SIMRUN"]==SimRun
     ExistingAMDat = ExistingAMDat[mask]
     ExistingAMDat = ExistingAMDat.merge(DatColMap,left_on = 'DATACOLLECTIONMEASUREMENT', right_on = 'DataColNo', how ='left')
     mask1 = ExistingAMDat.TIMEINT.isin(['900-4500']) 
@@ -63,11 +63,11 @@ Field_DatCol.rename(columns = {'AvgSpd':'ObsAvgSpd'},inplace=True)
 #*********************************************************************************
 # Specify Files
 #*********************************************************************************
-DatColFile_AM = r'./RawVissimOutput/20548_2019_am-existing_V8_Data Collection Results.att'
+DatColFile_AM = r'./RawVissimOutput/20548_2019_am-existing_V9_calib---2_Data Collection Results.att'
 file  = DatColFile_AM
 DatColFile_PM = DatColFile_AM
-DatCol_Existing_AM = PreProcessVissimDataCol(file = DatColFile_PM,DatColMap = DatColMap,Peak= "AM Peak")
-DatCol_Existing_PM = PreProcessVissimDataCol(file = DatColFile_PM,DatColMap = DatColMap,Peak= "PM Peak")    
+DatCol_Existing_AM = PreProcessVissimDataCol(file = DatColFile_PM,DatColMap = DatColMap,Peak= "AM Peak",SimRun=1)
+DatCol_Existing_PM = PreProcessVissimDataCol(file = DatColFile_PM,DatColMap = DatColMap,Peak= "PM Peak",SimRun=1)    
 
 #*********************************************************************************
 # Call Function
@@ -84,3 +84,6 @@ OutFi = r'TT_DataCol_Calibration.xlsx'
 writer=pd.ExcelWriter(OutFi, mode= 'a')
 Calibration_Dat.to_excel(writer, "Speed_Calibration" ,na_rep='-')
 writer.save() 
+
+subprocess.Popen([OutFi],shell=True)  
+
